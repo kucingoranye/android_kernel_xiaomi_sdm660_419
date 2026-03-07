b2d269e96cc7ad9c120ff1b12d12c247d810089a
kucingoranye <kucingoranye@github.com>
2026-03-08 05:35:46 +0700

    Optional Extras: selinux avc log spoofing
    Implement selinux avc log spoofing for whyred and lavender
    from https://github.com/backslashxx/KernelSU/issues/29
    
    Co-authored-by: backslashxx <118538522+backslashxx@users.noreply.github.com>
    Signed-off-by: kucingoranye <ardiantow04@gmail.com>

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -765,6 +765,10 @@ static void avc_audit_post_callback(struct audit_buffer *ab, void *a)
 			 ad->selinux_audit_data->result ? 0 : 1);
 }
 
+#ifdef CONFIG_KSU
+extern int ksu_handle_slow_avc_audit_new(u32 tsid, u16 *tclass);
+#endif
+
 /* This is the slow part of avc audit with big stack footprint */
 noinline int slow_avc_audit(struct selinux_state *state,
 			    u32 ssid, u32 tsid, u16 tclass,
@@ -777,6 +781,11 @@ noinline int slow_avc_audit(struct selinux_state *state,
 	if (!denied)
 		return 0;
 
+#ifdef CONFIG_KSU
+	ksu_handle_slow_avc_audit_new(tsid, &tclass);
+	if (!tclass)
+		return 0;
+#endif
 	if (!a) {
 		a = &stack_data;
 		a->type = LSM_AUDIT_DATA_NONE;
